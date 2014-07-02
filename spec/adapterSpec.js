27d4469990de3e0aeb2fab45612b89084923c7df
Julie Ralph <ju.ralph@gmail.com>
2014-07-01 17:28:56 -0700

    fix(tests): fix failing tests due to accidental commenting

--- spec/adapterSpec.js
+++ spec/adapterSpec.js
@@ -95,101 +95,97 @@ describe('webdriverJS Jasmine adapter', function() {
     });
   });
 
-  // beforeEach(function() {
-  //   fakeDriver.setUp().then(function(value) {
-  //     console.log('This should print before each test: ' + value);
-  //   });
-  // });
+  beforeEach(function() {
+    fakeDriver.setUp().then(function(value) {
+      console.log('This should print before each test: ' + value);
+    });
+  });
 
-  // it('should pass normal synchronous tests', function() {
-  //   expect(true).toEqual(true);
-  // });
+  it('should pass normal synchronous tests', function() {
+    expect(true).toEqual(true);
+  });
 
-  // it('should compare a promise to a primitive', function() {
-  //   expect(fakeDriver.getValueA()).toEqual('a');
-  //   expect(fakeDriver.getValueB()).toEqual('b');
-  // });
+  it('should compare a promise to a primitive', function() {
+    expect(fakeDriver.getValueA()).toEqual('a');
+    expect(fakeDriver.getValueB()).toEqual('b');
+  });
 
-  // it('should wait till the expect to run the flow', function() {
-  //   var promiseA = fakeDriver.getValueA();
-  //   expect(promiseA.isPending()).toBe(true);
-  //   expect(promiseA).toEqual('a');
-  //   expect(promiseA.isPending()).toBe(true);
-  // });
+  it('should wait till the expect to run the flow', function() {
+    var promiseA = fakeDriver.getValueA();
+    expect(promiseA.isPending()).toBe(true);
+    expect(promiseA).toEqual('a');
+    expect(promiseA.isPending()).toBe(true);
+  });
 
-  // it('should compare a promise to a promise', function() {
-  //   expect(fakeDriver.getValueA()).toEqual(fakeDriver.getOtherValueA());
-  // });
+  it('should compare a promise to a promise', function() {
+    expect(fakeDriver.getValueA()).toEqual(fakeDriver.getOtherValueA());
+  });
 
-  // it('should still allow use of the underlying promise', function() {
-  //   var promiseA = fakeDriver.getValueA();
-  //   promiseA.then(function(value) {
-  //     expect(value).toEqual('a');
-  //   });
-  // });
+  it('should still allow use of the underlying promise', function() {
+    var promiseA = fakeDriver.getValueA();
+    promiseA.then(function(value) {
+      expect(value).toEqual('a');
+    });
+  });
 
-  // it('should allow scheduling of tasks', function() {
-  //   fakeDriver.sleep(300);
-  //   expect(fakeDriver.getValueB()).toEqual('b');
-  // });
+  it('should allow scheduling of tasks', function() {
+    fakeDriver.sleep(300);
+    expect(fakeDriver.getValueB()).toEqual('b');
+  });
 
-  // it('should allow the use of custom matchers', function() {
-  //   expect(500).toBeLotsMoreThan(3);
-  //   expect(fakeDriver.getBigNumber()).toBeLotsMoreThan(33);
-  // });
+  it('should allow the use of custom matchers', function() {
+    expect(500).toBeLotsMoreThan(3);
+    expect(fakeDriver.getBigNumber()).toBeLotsMoreThan(33);
+  });
 
-  // it('should allow custom matchers to return a promise', function() {
-  //   expect(fakeDriver.getDisplayedElement()).toBeDisplayed();
-  //   expect(fakeDriver.getHiddenElement()).not.toBeDisplayed();
-  // });
+  it('should allow custom matchers to return a promise', function() {
+    expect(fakeDriver.getDisplayedElement()).toBeDisplayed();
+    expect(fakeDriver.getHiddenElement()).not.toBeDisplayed();
+  });
 
-  // it('should pass multiple arguments to matcher', function() {
-  //     // Passing specific precision
-  //     expect(fakeDriver.getDecimalNumber()).toBeCloseTo(3.1, 1);
-  //     expect(fakeDriver.getDecimalNumber()).not.toBeCloseTo(3.1, 2);
+  it('should pass multiple arguments to matcher', function() {
+      // Passing specific precision
+      expect(fakeDriver.getDecimalNumber()).toBeCloseTo(3.1, 1);
+      expect(fakeDriver.getDecimalNumber()).not.toBeCloseTo(3.1, 2);
 
-  //     // Using default precision (2)
-  //     expect(fakeDriver.getDecimalNumber()).not.toBeCloseTo(3.1);
-  //     expect(fakeDriver.getDecimalNumber()).toBeCloseTo(3.14);
-  //   });
+      // Using default precision (2)
+      expect(fakeDriver.getDecimalNumber()).not.toBeCloseTo(3.1);
+      expect(fakeDriver.getDecimalNumber()).toBeCloseTo(3.14);
+    });
 
-  // describe('not', function() {
-  //   it('should still pass normal synchronous tests', function() {
-  //     expect(4).not.toEqual(5);
-  //   });
+  describe('not', function() {
+    it('should still pass normal synchronous tests', function() {
+      expect(4).not.toEqual(5);
+    });
 
-  //   it('should compare a promise to a primitive', function() {
-  //     expect(fakeDriver.getValueA()).not.toEqual('b');
-  //   });
+    it('should compare a promise to a primitive', function() {
+      expect(fakeDriver.getValueA()).not.toEqual('b');
+    });
 
-  //   it('should compare a promise to a promise', function() {
-  //     expect(fakeDriver.getValueA()).not.toEqual(fakeDriver.getValueB());
-  //   });
-  // });
+    it('should compare a promise to a promise', function() {
+      expect(fakeDriver.getValueA()).not.toEqual(fakeDriver.getValueB());
+    });
+  });
 
-  // it('should throw an error with a WebElement actual value', function() {
-  //   var webElement = new webdriver.WebElement(fakeDriver, 'idstring');
+  it('should throw an error with a WebElement actual value', function() {
+    var webElement = new webdriver.WebElement(fakeDriver, 'idstring');
 
-  //   expect(function() {
-  //     expect(webElement).toEqual(4);
-  //   }).toThrow('expect called with WebElement argument, expected a Promise. ' +
-  //       'Did you mean to use .getText()?');
-  // });
+    expect(function() {
+      expect(webElement).toEqual(4);
+    }).toThrow('expect called with WebElement argument, expected a Promise. ' +
+        'Did you mean to use .getText()?');
+  });
 
   // Uncomment to see timeout failures.
 
-  it('should timeout after 200ms', function() {
-    expect(fakeDriver.getValueA()).toEqual('a');
-    // This one is still lingering in the 'getHistory' of the next one.
-    // That's OK though, right?
-    // Or should the history be cleared?
-    // History = pending tasks + flow.history_
-  }, 300);
+  // it('should timeout after 200ms', function() {
+  //   expect(fakeDriver.getValueA()).toEqual('a');
+  // }, 300);
 
-  it('should timeout after 300ms', function() {
-    fakeDriver.sleep(9999);
-    expect(fakeDriver.getValueB()).toEqual('b');
-  }, 300);
+  // it('should timeout after 300ms', function() {
+  //   fakeDriver.sleep(9999);
+  //   expect(fakeDriver.getValueB()).toEqual('b');
+  // }, 300);
 
   // it('should pass errors from done callback', function(done) {
   //   done('an error');
