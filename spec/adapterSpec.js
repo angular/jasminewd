8f8b8b39e779559fd3b29b138d7577658b8a64b7
Julie Ralph <ju.ralph@gmail.com>
2015-04-13 17:55:07 -0700

    tests(context): test that the `this` variable points to the right thing
    Note: this means that using `this.addMatchers` no longer works inside
    before blocks or specs. It should have been changed to `jamsine.addMatchers`
    since the upgrade to Jasmine 2. It was still working by accident up until
    the previous commit.

--- spec/adapterSpec.js
+++ spec/adapterSpec.js
@@ -16,6 +16,21 @@ describe('webdriverJS Jasmine adapter plain', function() {
   });
 });
 
+describe('context', function() {
+  beforeEach(function() {
+    this.foo = 0;
+  });
+
+  it('can use the `this` to share state', function() {
+    expect(this.foo).toEqual(0);
+    this.bar = 'test pollution?';
+  });
+
+  it('prevents test pollution by having an empty `this` created for the next spec', function() {
+    expect(this.foo).toEqual(0);
+    expect(this.bar).toBe(undefined);
+  });
+});
 
 describe('webdriverJS Jasmine adapter', function() {
   // Shorten this and you should see tests timing out.
