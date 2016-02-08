550dd96e3833729e8678e54b31f40827019700d6
Julie Ralph <ju.ralph@gmail.com>
2016-02-08 13:51:02 -0800

    chore(test): print command line

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ CMD_BASE="node node_modules/.bin/jasmine JASMINE_CONFIG_PATH="
 
 echo "### running passing specs"
 CMD=$CMD_BASE$PASSING_SPECS
+echo "### $CMD"
 $CMD
 [ "$?" -eq 0 ] || exit 1
 echo
@@ -11,6 +12,7 @@ echo
 EXPECTED_RESULTS="14 specs, 13 failures"
 echo "### running failing specs (expecting $EXPECTED_RESULTS)"
 CMD=$CMD_BASE$FAILING_SPECS
+echo "### $CMD"
 res=`$CMD 2>/dev/null`
 results_line=`echo "$res" | tail -2 | head -1`
 echo "result: $results_line"
