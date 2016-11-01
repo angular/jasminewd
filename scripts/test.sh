393cf83238ad96c75bc45a3054da4f4b6e88ca51
Sammy Jelin <sjelin@gmail.com>
2016-11-01 14:47:52 -0700

    feat(async/await): Support overwriting native promise with WebDriver's version
    See https://github.com/angular/jasminewd/issues/61

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,6 @@
 PASSING_SPECS="spec/support/passing_specs.json"
 FAILING_SPECS="spec/support/failing_specs.json"
+EXPERIMENTAL_SPECS="spec/support/experimental_specs.json"
 CMD_BASE="node node_modules/.bin/jasmine JASMINE_CONFIG_PATH="
 
 echo "### running passing specs"
@@ -17,5 +18,13 @@ res=`$CMD 2>/dev/null`
 results_line=`echo "$res" | tail -2 | head -1`
 echo "result: $results_line"
 [ "$results_line" = "$EXPECTED_RESULTS" ] || exit 1
+
+echo "### running experimental specs"
+CMD=$CMD_BASE$EXPERIMENTAL_SPECS
+echo "### $CMD"
+$CMD
+# We don't care if the experimental specs pass.
+# [ "$?" -eq 0 ] || exit 1
+echo
 
 echo "all pass"
